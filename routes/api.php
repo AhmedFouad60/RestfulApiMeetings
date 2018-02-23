82a6fde71c428e4f7c5bf510bb8a6ef7edfb4cf6
AhmedFouad60 <lionmoon60@yahoo.com>
2018-02-23 23:04:11 +0200

    test the controller Methods with PostMan

--- routes/api.php
+++ routes/api.php
@@ -13,13 +13,13 @@ use Illuminate\Http\Request;
 |
 */
 
-Route::group(['prefix'=>'api/v1'],function (){
+Route::group(['prefix'=>'/v1'],function (){
 
     Route::resource('meeting','MeetingController',[
         'except' =>['edit','create']
     ]);
 
-    Route::resource('meeting/registration','RegisterationController',[
+    Route::resource('meeting/registration','RegistrationController',[
         'only'=>['store','destroy']
     ]);
 
